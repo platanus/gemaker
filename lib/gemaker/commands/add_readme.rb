b229949c78498887b8b6602d446a7632e4ff2216
Leandro Segovia <ldlsegovia@gmail.com>
2016-09-02 12:13:44 -0300

    feat(): remove README.rdoc

--- lib/gemaker/commands/add_readme.rb
+++ lib/gemaker/commands/add_readme.rb
@@ -3,6 +3,7 @@ module Gemaker
     class AddReadme < Gemaker::Cmd::Base
       def perform
         copy_template("README.md", "README.md", config: @config)
+        rm_rf("README.rdoc")
         puts "Replace README.md"
       end
     end
