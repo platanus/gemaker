6fcd61ca9f9b6438da3d0699167f8a0fb576a807
Leandro Segovia <ldlsegovia@gmail.com>
2016-10-12 18:33:21 -0300

    feat(): add example class

--- lib/gemaker/commands/customize_engine.rb
+++ lib/gemaker/commands/customize_engine.rb
@@ -2,7 +2,10 @@ module Gemaker
   module Cmd
     class CustomizeEngine < Gemaker::Cmd::Base
       def in_engine_context
-        copy_template("engine/engine.rb", "lib/#{@config.gem_name}/engine.rb", config: @config)
+        copy_template("engine/engine.rb",
+          "lib/#{@config.gem_name}/engine.rb", config: @config)
+        copy_template("engine/example_class.rb",
+          "lib/#{@config.gem_name}/example_class.rb", config: @config)
         info "Customize engine.rb"
       end
     end
