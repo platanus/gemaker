32c462c2ecb3f3d37235ed289e7abd35256aa503
Leandro Segovia <ldlsegovia@gmail.com>
2021-07-21 13:46:19 -0300

    feat(): remove coveralls configuration related to travis

--- lib/gemaker/commands/configure_test_env.rb
+++ lib/gemaker/commands/configure_test_env.rb
@@ -4,7 +4,6 @@ module Gemaker
       def in_normal_context
         copy_template("normal/spec_helper.rb", "spec/spec_helper.rb", config: @config)
         copy_file("test_helpers.rb", "spec/support/test_helpers.rb")
-        copy_file("coveralls.yml", ".coveralls.yml")
         copy_template("normal/test_example.rb", "spec/#{@config.gem_name}_spec.rb", config: @config)
         copy_file("normal/Guardfile", "Guardfile")
         info "Configure Rspec"
@@ -17,7 +16,6 @@ module Gemaker
           "spec/dummy/spec/#{@config.gem_name}_spec.rb", config: @config)
         create_dir("spec/dummy/spec/factories")
         copy_file("test_helpers.rb", "spec/dummy/spec/support/test_helpers.rb")
-        copy_file("coveralls.yml", ".coveralls.yml")
         copy_file("image.png", "spec/dummy/spec/assets/image.png")
         copy_file("video.mp4", "spec/dummy/spec/assets/video.mp4")
         copy_file("engine/rspec", ".rspec")
